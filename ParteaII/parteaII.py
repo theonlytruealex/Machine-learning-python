0edc275b6dee0b2a6cc2c85ac1fffa25492cbcb2
theonlytruealex <alexiova264@gmail.com>
2024-05-27 14:37:54 +0300

    Added: Task 2 Remove outliers with Z-score

--- ParteaII/parteaII.py
+++ ParteaII/parteaII.py
@@ -3,9 +3,22 @@ import pandas as pd
 
 """ Task 1 """
 
+def removeOutliers(dataFrame, col):
+    [Q1, Q3] = dataFrame[col].quantile(q=[0.25, 0.75])
+    IQR = Q3 - Q1
+    return dataFrame.query('(@Q1 - 1.5 * @IQR) <= '+ col +' <= (@Q3 + 1.5 * @IQR)')
+
+""" Task 2"""
+
+def zScore(dataFrame, col):
+    stdDev = dataFrame[col].std()
+    mean = dataFrame[col].mean()
+    maxZ = 3 * stdDev + mean
+    minZ = -3 * stdDev + mean
+    return dataFrame.query('@minZ <= '+ col +' <= @maxZ')
+
+
 dataFrame = pd.read_csv('../DataForAll/train.csv')
-[Q1, Q3] = dataFrame.Age.quantile(q=[0.25, 0.75])
-IQR = Q3 - Q1
-print (Q1 - 1.5 * IQR, Q3 + 1.5 * IQR)
-noOutDf = dataFrame.query('(@Q1 - 1.5 * @IQR) <= Age <= (@Q3 + 1.5 * @IQR)')
-print(noOutDf["Age"].max(), noOutDf["Age"].min())
+col = "Age"
+newDataFrame = removeOutliers(dataFrame, col)
+newerDataFrame = zScore(dataFrame, col)
