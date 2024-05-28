e6934a7c16edf0ac287b514df338617c79392e17
theonlytruealex <alexiova264@gmail.com>
2024-05-28 23:55:51 +0300

    Added: normalization Edited: split dataset before modifyingtraining set

--- ParteaII/parteaII.py
+++ ParteaII/parteaII.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+import sklearn.preprocessing as pk
 
 """ Task 1 """
 
@@ -21,17 +22,25 @@ def zScore(dataFrame, col = "Age", zFactor = 3):
 
 """ Task 4 """
 
-""" Subtask 2- clean up the data: I want it cleaned up before splitting """
-unsplitDf = pd.read_csv('../DataForAll/train.csv')
-""" unsplitDf = unsplitDf.drop('Cabin', axis=1) """
-unsplitDf.replace({'male': 0, 'female' : 1}, inplace=True)
-unsplitDf.replace({'C': 0, 'Q' : 1, 'S': 2}, inplace=True)
-unsplitDf["Age"].fillna(unsplitDf.Age.mean(), inplace=True)
-unsplitDf["Embarked"].fillna(unsplitDf.Embarked.median(), inplace=True)
-
 """ Subtask 1 - split dataset """
+
+unsplitDf = pd.read_csv('../DataForAll/train.csv')
 trainSet = unsplitDf.iloc[:int(0.8 * len(unsplitDf)),:]
 testSet = unsplitDf.iloc[int(0.8 * len(unsplitDf)):,:]
-print(trainSet, testSet)
+
+""" Subtask 2- clean up the data: I want it cleaned up before normalization"""
+trainSet = removeOutliers(trainSet)
+trainSet = removeOutliers(trainSet, "Fare")
+trainSet.replace({'male': 0, 'female' : 1}, inplace=True)
+trainSet.replace({'C': 0, 'Q' : 1, 'S': 2}, inplace=True)
+trainSet["Age"].fillna(trainSet.Age.mean(), inplace=True)
+trainSet["Embarked"].fillna(trainSet.Embarked.median(), inplace=True)
+
+""" Normalize data """
+normalizedDf = trainSet
+normalizedDf['Age'] = pk.minmax_scale(trainSet['Age'])
+normalizedDf['Fare'] = pk.minmax_scale(trainSet['Fare'])
+
+print(normalizedDf)
 
 """ Subtask 3 - """
