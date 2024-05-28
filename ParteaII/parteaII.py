a2ae9cebe719585ca24dc014a316c56f25ab25bc
theonlytruealex <alexiova264@gmail.com>
2024-05-28 16:44:26 +0300

    Added: Task 4 St 1 & 2 Cleaned up the database and split dataset

--- ParteaII/parteaII.py
+++ ParteaII/parteaII.py
@@ -3,22 +3,35 @@ import pandas as pd
 
 """ Task 1 """
 
-def removeOutliers(dataFrame, col):
+def removeOutliers(dataFrame, col = "Age"):
     [Q1, Q3] = dataFrame[col].quantile(q=[0.25, 0.75])
     IQR = Q3 - Q1
     return dataFrame.query('(@Q1 - 1.5 * @IQR) <= '+ col +' <= (@Q3 + 1.5 * @IQR)')
 
-""" Task 2"""
+""" Task 2 """
 
-def zScore(dataFrame, col):
+def zScore(dataFrame, col = "Age", zFactor = 3):
     stdDev = dataFrame[col].std()
     mean = dataFrame[col].mean()
-    maxZ = 3 * stdDev + mean
-    minZ = -3 * stdDev + mean
+    maxZ = zFactor * stdDev + mean
+    minZ = -zFactor * stdDev + mean
     return dataFrame.query('@minZ <= '+ col +' <= @maxZ')
 
+""" Task 3 """
 
-dataFrame = pd.read_csv('../DataForAll/train.csv')
-col = "Age"
-newDataFrame = removeOutliers(dataFrame, col)
-newerDataFrame = zScore(dataFrame, col)
+""" Task 4 """
+
+""" Subtask 2- clean up the data: I want it cleaned up before splitting """
+unsplitDf = pd.read_csv('../DataForAll/train.csv')
+""" unsplitDf = unsplitDf.drop('Cabin', axis=1) """
+unsplitDf.replace({'male': 0, 'female' : 1}, inplace=True)
+unsplitDf.replace({'C': 0, 'Q' : 1, 'S': 2}, inplace=True)
+unsplitDf["Age"].fillna(unsplitDf.Age.mean(), inplace=True)
+unsplitDf["Embarked"].fillna(unsplitDf.Embarked.median(), inplace=True)
+
+""" Subtask 1 - split dataset """
+trainSet = unsplitDf.iloc[:int(0.8 * len(unsplitDf)),:]
+testSet = unsplitDf.iloc[int(0.8 * len(unsplitDf)):,:]
+print(trainSet, testSet)
+
+""" Subtask 3 - """
